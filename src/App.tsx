7a89e7d032e9c771bd9f14c21d0e329800736935
Zevi Liao <jointjm@gmail.com>
2022-09-23 00:12:56 +0800

    add ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import './App.css';
+
+import ChildComp from "./components/ChildComp";
+
+function App() {
+  return (
+    <div className="App">
+      <ChildComp></ChildComp>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChildComp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChildComp = () => {
+  return (
+    <div>ChildComp</div>
+  )
+}
+
+export default ChildComp
