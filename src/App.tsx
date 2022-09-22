72e2f652e0a6fce597faaba6cd4615ed5a2a07be
Zevi Liao <jointjm@gmail.com>
2022-09-23 03:11:58 +0800

    test useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-// import './App.css';
-
-import ChildComp from "./components/ChildComp";
+import { useState, useCallback } from "react";
+import { ChildMemo } from "./components/ChildComp";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Child组件");
+
+  console.log("父组件?");
+
   return (
-    <div className="App">
-      <ChildComp></ChildComp>
-    </div>
+    <>
+      <button
+        onClick={(e) => {
+          setCount(count + 1);
+        }}
+      >
+        加1
+      </button>
+      <p>count:{count}</p>
+      <ChildMemo
+        name={name}
+        onClick={useCallback((newName: string) => setName(newName), [])}
+      />
+    </>
   );
 }
 
--- src/components/ChildComp.tsx
+++ src/components/ChildComp.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { memo } from 'react'
 
-const ChildComp = () => {
-  return (
-    <div>ChildComp</div>
-  )
+interface ChildProps {
+  name: string;
+  onClick: Function;
 }
 
-export default ChildComp
+const Child = ({ name, onClick}: ChildProps): JSX.Element => {
+  console.log('子组件?')
+  return(
+      <>
+          <div>我是一个子组件，父级传过来的数据：{name}</div>
+          <button onClick={onClick.bind(null, '新的子组件name')}>改变name</button>
+      </>
+  );
+}
+
+// export const ChildMemo = memo(Child);
+export const ChildMemo = Child;
