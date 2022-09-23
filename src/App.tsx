0f7e9fa9b8463aae1cd5f11aea42831580dab4f6
Zevi Liao <jointjm@gmail.com>
2022-09-24 03:52:52 +0800

    sol-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,22 @@
 import { useEffect, useRef } from "react";
-// import { range, filter, map } from "rxjs";
-import { Subject } from "rxjs";
+import { fromEvent } from "rxjs";
 
 function App() {
-  const btnRef = useRef<HTMLButtonElement>(null);
-  const data$ = new Subject(); // TV
-  const test = () => {
-    data$.subscribe((value) => {
-      console.log(value);
-    });
-
-    // data$.next(1); // 送節目。
-    // range(1, 10)
-    //   .pipe(
-    //     filter((x) => x % 2 === 1),
-    //     map((x) => x + x)
-    //   )
-    //   .subscribe((x) => console.log(x));
+  const buttonHandler = (buttonRef: any) => {
+    const data$ = fromEvent(buttonRef, "click");
+    data$.subscribe((v) => console.log("click"));
   };
-
-  const btnClick = () => {
-    data$.next("hello");
-  };
+  const test = () => {};
 
   useEffect(() => {
-    const btn = btnRef.current;
-    btn?.addEventListener("click", btnClick);
-    test();
+    // test();
 
-    return () => {
-      btn?.removeEventListener("click", btnClick);
-    };
+    return () => {};
   }, []);
 
   return (
     <div style={{ padding: "15px" }}>
-      <button ref={btnRef}>hello</button>
+      <button ref={buttonHandler}>hello</button>
     </div>
   );
 }
