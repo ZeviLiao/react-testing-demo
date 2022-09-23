04435e5ad7203c784bcf21b5552bd166f5980f57
Zevi Liao <jointjm@gmail.com>
2022-09-24 05:02:14 +0800

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { useEffect, useRef } from "react";
 import { fromEvent, Subject } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, filter } from "rxjs/operators";
 
 function App() {
   const ref = useRef(null);
   const test = () => {
+    // create stream.
     const click$ = fromEvent(ref.current as any, "click");
 
-    const counter2$ = click$.pipe(
-      map((o: any, i) => {
-        // console.log(o.x, o.y);
-        console.log(i + 1 + "-zevi");
-      })
-    );
+    // from another stream.
+    const counter2$ = click$.pipe(map((o: any, i) => i + 1));
 
+    const eventCtr$ = counter2$.pipe(filter((v) => v % 2 === 0));
+
+    // subscribe stream.
     click$.subscribe();
-
-    counter2$.subscribe();
+    counter2$.subscribe((v) => console.log(v));
+    eventCtr$.subscribe((v) => console.log("---", v));
   };
 
   useEffect(() => {
