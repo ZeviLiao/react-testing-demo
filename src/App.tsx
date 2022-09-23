dc8c32702630ef3aa8c6ed7f7abf77398fb9531e
Zevi Liao <jointjm@gmail.com>
2022-09-24 02:46:30 +0800

    addEventListener

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import { useEffect } from "react";
-import { range, filter, map } from "rxjs";
+import { useEffect, useRef } from "react";
+// import { range, filter, map } from "rxjs";
+import { Subject } from "rxjs";
 
 function App() {
+  const btnRef = useRef<HTMLButtonElement>(null);
   const test = () => {
-    range(1, 10)
-      .pipe(
-        filter((x) => x % 2 === 1),
-        map((x) => x + x)
-      )
-      .subscribe((x) => console.log(x));
+    const data$ = new Subject(); // TV
+
+    data$.subscribe((value) => console.log(value));
+
+    data$.next(1); // 送節目。
+    data$.next(2);
+    data$.next(3);
+    data$.next(4);
+    // range(1, 10)
+    //   .pipe(
+    //     filter((x) => x % 2 === 1),
+    //     map((x) => x + x)
+    //   )
+    //   .subscribe((x) => console.log(x));
   };
 
+  const btnClick = () => console.log("hello");
+
   useEffect(() => {
+    const btn = btnRef.current;
+    btn?.addEventListener("click", btnClick);
     test();
+
+    return () => {
+      btn?.removeEventListener("click", btnClick);
+    };
   }, []);
 
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div style={{ padding: "15px" }}>
+      <button ref={btnRef}>hello</button>
+    </div>
   );
 }
 
